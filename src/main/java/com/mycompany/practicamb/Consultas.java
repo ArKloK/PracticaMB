050c1de6e816a9e5e5437cec94a40724b5463d64
Carlos <Carlos@DESKTOP-B7K44QQ>
2022-11-08 20:19:39 +0100

    v2

--- src/main/java/com/mycompany/practicamb/Consultas.java
+++ src/main/java/com/mycompany/practicamb/Consultas.java
@@ -12,7 +12,7 @@ import org.apache.solr.common.SolrDocumentList;
 public class Consultas {
 
     public static void main(String[] args) throws SolrServerException, IOException {
-        String fileName = "src\\main\\java\\ejemplo\\consultas.txt"; //Ruta relativa hacia el fichero
+        String fileName = "src\\main\\java\\ejemplo\\CISI.QRY"; //Ruta relativa hacia el fichero
         Scanner scan = new Scanner(new File(fileName));
         String texto = "";
 
@@ -20,28 +20,27 @@ public class Consultas {
 
         while (scan.hasNext()) {
             String line = scan.nextLine();
+
             if (line.equals(".W")) {
-                while (scan.hasNext() && !line.equals(".I")) {
-                    //Controlando que elija las lineas que contengan .W o terminen por . o por ? siempre y cuando haya una siguiente linea para seguir iterando
-                    if ((line.equals(".W") || line.endsWith("?") || line.endsWith(".")) && scan.hasNext()) {
-                        //Bucle para elegir las 5 primeras palabras de la consulta
-                        for (int j = 0; j < 5; j++) {
-                            line = scan.next();
-                            texto += " " + line;
-                        }
-                        //Cuerpo de la consulta y mostrando los resultados
-                        SolrQuery query = new SolrQuery();
-                        query.setQuery("texto:" + texto);
-                        query.setFields("id");
-                        QueryResponse rsp = solr.query(query);
-                        SolrDocumentList docs = rsp.getResults();
-                        for (int i = 0; i < docs.size(); ++i) {
-                            System.out.println(docs.get(i));
-                        }
-                        texto = "";
+                for (int j = 0; j < 5; j++) {
+                    line = scan.next();
+                    if (line.startsWith("(")) {
+                        line = line.substring(1);
                     }
-                    line = scan.nextLine();
+                    if (line.endsWith(")")) {
+                        line = line.substring(0, line.length()-1);
+                    }
+                    texto += " " + line;
                 }
+                SolrQuery query = new SolrQuery();
+                query.setQuery("texto:" + texto);
+                query.setFields("id");
+                QueryResponse rsp = solr.query(query);
+                SolrDocumentList docs = rsp.getResults();
+                for (int i = 0; i < docs.size(); ++i) {
+                    System.out.println(docs.get(i));
+                }
+                texto = "";
             }
         }
     }
